65c28708dd815637312eb4e240bc6f7e5cc37560
chenjh <chenjh@jpush.cn>
2018-05-28 10:05:44 +0800

    amazon move

--- models/admin/AdminInit.go
+++ models/admin/AdminInit.go
@@ -19,6 +19,7 @@ import (
 	"github.com/astaxie/beego"
 	"github.com/hunterhug/rabbit/lib"
 	"github.com/hunterhug/rabbit/models/blog"
+	"github.com/hunterhug/rabbit/conf"
 )
 
 func InitData() {
@@ -38,7 +39,7 @@ func InsertConfig() {
 	c := new(blog.Config)
 	c.Photo = "/file/image/46/68756e746572687567aadc0c7438bb9e28d2c4eeaa310828e8.png"
 	c.Id = 1
-	c.Title = "Rabbit(Tuzi) Enterprise Web"
+	c.Title = "大数据智能平台"
 	c.Webinfo = `
 	{
 		"1":{"name":"About","limit":6},
@@ -155,10 +156,10 @@ func InsertUser() {
 	fmt.Println("insert user ...")
 	u := new(User)
 	u.Username = beego.AppConfig.String("rbac_admin_user")
-	u.Nickname = "TuziAdmin"
+	u.Nickname = "Admin"
 	u.Password = lib.Pwdhash(beego.AppConfig.String("rbac_admin_user"))
-	u.Email = "569929309@qq.com"
-	u.Remark = "God in Rabbit Country"
+	u.Email = "459527502@qq.com"
+	u.Remark = "最高权限的王"
 	// 2 stand for close, but it has very high authority
 	u.Status = 2
 	u.Createtime = lib.GetTime()
@@ -169,9 +170,9 @@ func InsertUser() {
 
 	u1 := new(User)
 	u1.Username = "test"
-	u1.Nickname = "TuziTest"
+	u1.Nickname = "Test"
 	u1.Password = lib.Pwdhash("test")
-	u1.Email = "569929309@qq.com"
+	u1.Email = "459527502@qq.com"
 	u1.Remark = "Just a Test User"
 	u1.Status = 1
 	u1.Createtime = lib.GetTime()
@@ -217,6 +218,52 @@ func InsertGroup() {
 		fmt.Println(e.Error())
 	}
 	fmt.Println("insert group end")
+
+	if conf.Amazon {
+		gb := new(Group)
+		gb.Name = "美国亚马逊"
+		gb.Title = "美国亚马逊"
+		gb.Sort = 4
+		gb.Id = 4
+		gb.Status = 1
+		e = gb.Insert()
+		if e != nil {
+			fmt.Println(e.Error())
+		}
+
+		jgb := new(Group)
+		jgb.Name = "日本亚马逊"
+		jgb.Title = "日本亚马逊"
+		jgb.Sort = 5
+		jgb.Id = 5
+		jgb.Status = 1
+		e = jgb.Insert()
+		if e != nil {
+			fmt.Println(e.Error())
+		}
+
+		de := new(Group)
+		de.Name = "德国亚马逊"
+		de.Title = "德国亚马逊"
+		de.Sort = 6
+		de.Id = 6
+		de.Status = 1
+		e = de.Insert()
+		if e != nil {
+			fmt.Println(e.Error())
+		}
+
+		uk := new(Group)
+		uk.Name = "英国亚马逊"
+		uk.Title = "英国亚马逊"
+		uk.Sort = 7
+		uk.Id = 7
+		uk.Status = 1
+		e = uk.Insert()
+		if e != nil {
+			fmt.Println(e.Error())
+		}
+	}
 }
 
 // 角色数据
@@ -352,6 +399,125 @@ func InsertNodes() {
 		{Id: 62, Name: "AddRoll", Title: "增加轮转", Remark: "", Level: 3, Pid: 60, Status: 1, Group: g},
 		{Id: 63, Name: "UpdateRoll", Title: "更新轮转", Remark: "", Level: 3, Pid: 60, Status: 1, Group: g},
 		{Id: 64, Name: "DeleteRoll", Title: "删除轮转", Remark: "", Level: 3, Pid: 60, Status: 1, Group: g},
+	}
+
+	if conf.Amazon {
+		gb := new(Group)
+		gb.Id = 4
+
+		jp := new(Group)
+		jp.Id = 5
+
+		de := new(Group)
+		de.Id = 6
+
+		uk := new(Group)
+		uk.Id = 7
+
+		amazonnode := []Node{
+			{Id: 65, Name: "auas", Title: "基础数据", Remark: "", Level: 1, Pid: 0, Status: 1, Group: gb},
+			{Id: 66, Name: "base/index", Title: "小类数据", Remark: "", Level: 2, Pid: 65, Status: 1, Group: gb},
+			{Id: 67, Name: "Index", Title: "美国站小类数据列表", Remark: "", Level: 3, Pid: 66, Status: 1, Group: gb},
+			{Id: 671, Name: "Query", Title: "美国站小类数据查询", Remark: "", Level: 3, Pid: 66, Status: 1, Group: gb},
+			{Id: 672, Name: "Export", Title: "美国站小类数据导出", Remark: "", Level: 3, Pid: 66, Status: 1, Group: gb},
+
+			{Id: 68, Name: "big/index", Title: "大类数据", Remark: "", Level: 2, Pid: 65, Status: 1, Group: gb},
+			{Id: 69, Name: "Index", Title: "美国站大类数据列表", Remark: "", Level: 3, Pid: 68, Status: 1, Group: gb},
+			{Id: 70, Name: "Query", Title: "美国站大类数据查询", Remark: "", Level: 3, Pid: 68, Status: 1, Group: gb},
+			{Id: 71, Name: "Export", Title: "美国站大类数据导出", Remark: "", Level: 3, Pid: 68, Status: 1, Group: gb},
+			{Id: 711, Name: "Asin", Title: "美国站Asin历史记录", Remark: "", Level: 3, Pid: 68, Status: 1, Group: gb},
+
+			{Id: 72, Name: "asin/index", Title: "Asin数据", Remark: "", Level: 2, Pid: 65, Status: 1, Group: gb},
+			{Id: 73, Name: "Index", Title: "美国站Asin数据列表", Remark: "", Level: 3, Pid: 72, Status: 1, Group: gb},
+			{Id: 74, Name: "Query", Title: "美国站Asin数据查询", Remark: "", Level: 3, Pid: 72, Status: 1, Group: gb},
+			{Id: 75, Name: "Export", Title: "美国站Asin数据导出", Remark: "", Level: 3, Pid: 72, Status: 1, Group: gb},
+
+			{Id: 76, Name: "url/index", Title: "类目数据", Remark: "", Level: 2, Pid: 65, Status: 1, Group: gb},
+			{Id: 77, Name: "Index", Title: "美国站类目数据列表", Remark: "", Level: 3, Pid: 76, Status: 1, Group: gb},
+			{Id: 78, Name: "Query", Title: "美国站类目数据查询", Remark: "", Level: 3, Pid: 76, Status: 1, Group: gb},
+			{Id: 79, Name: "Update", Title: "美国站类目数据更新", Remark: "", Level: 3, Pid: 76, Status: 1, Group: gb},
+
+			{Id: 89, Name: "csv", Title: "业务数据", Remark: "", Level: 1, Pid: 0, Status: 1, Group: gb},
+			{Id: 90, Name: "report/index", Title: "报告数据", Remark: "", Level: 2, Pid: 89, Status: 1, Group: gb},
+			{Id: 91, Name: "Index", Title: "美国站报告数据列表", Remark: "", Level: 3, Pid: 90, Status: 1, Group: gb},
+			{Id: 92, Name: "Query", Title: "美国站报告数据查询", Remark: "", Level: 3, Pid: 90, Status: 1, Group: gb},
+			{Id: 93, Name: "Export", Title: "美国站报告数据导出", Remark: "", Level: 3, Pid: 90, Status: 1, Group: gb},
+			{Id: 94, Name: "Import", Title: "美国站报告数据导入", Remark: "", Level: 3, Pid: 90, Status: 1, Group: gb},
+			{Id: 95, Name: "Delete", Title: "美国站报告数据删除", Remark: "", Level: 3, Pid: 90, Status: 1, Group: gb},
+
+			//---------------------------------------------------
+			//智干亚马逊日本数据
+			{Id: 102, Name: "ajp", Title: "基础数据", Remark: "", Level: 1, Pid: 0, Status: 1, Group: jp},
+			{Id: 103, Name: "base/index", Title: "小类数据", Remark: "", Level: 2, Pid: 102, Status: 1, Group: jp},
+			{Id: 104, Name: "Index", Title: "日本站小类数据列表", Remark: "", Level: 3, Pid: 103, Status: 1, Group: jp},
+			{Id: 105, Name: "Query", Title: "日本站小类数据查询", Remark: "", Level: 3, Pid: 103, Status: 1, Group: jp},
+			{Id: 106, Name: "Export", Title: "日本站小类数据导出", Remark: "", Level: 3, Pid: 103, Status: 1, Group: jp},
+
+			{Id: 107, Name: "big/index", Title: "大类数据", Remark: "", Level: 2, Pid: 102, Status: 1, Group: jp},
+			{Id: 108, Name: "Index", Title: "日本站大类数据列表", Remark: "", Level: 3, Pid: 107, Status: 1, Group: jp},
+			{Id: 109, Name: "Query", Title: "日本站大类数据查询", Remark: "", Level: 3, Pid: 107, Status: 1, Group: jp},
+			{Id: 110, Name: "Export", Title: "日本站大类数据导出", Remark: "", Level: 3, Pid: 107, Status: 1, Group: jp},
+			{Id: 712, Name: "Asin", Title: "日本站Asin历史记录", Remark: "", Level: 3, Pid: 107, Status: 1, Group: jp},
+
+			{Id: 111, Name: "asin/index", Title: "Asin数据", Remark: "", Level: 2, Pid: 102, Status: 1, Group: jp},
+			{Id: 112, Name: "Index", Title: "日本站Asin数据列表", Remark: "", Level: 3, Pid: 111, Status: 1, Group: jp},
+			{Id: 113, Name: "Query", Title: "日本站Asin数据查询", Remark: "", Level: 3, Pid: 111, Status: 1, Group: jp},
+			{Id: 114, Name: "Export", Title: "日本站Asin数据导出", Remark: "", Level: 3, Pid: 111, Status: 1, Group: jp},
+
+			{Id: 115, Name: "url/index", Title: "类目数据", Remark: "", Level: 2, Pid: 102, Status: 1, Group: jp},
+			{Id: 116, Name: "Index", Title: "日本站类目数据列表", Remark: "", Level: 3, Pid: 115, Status: 1, Group: jp},
+			{Id: 117, Name: "Query", Title: "日本站类目数据查询", Remark: "", Level: 3, Pid: 115, Status: 1, Group: jp},
+			{Id: 118, Name: "Update", Title: "日本站类目数据更新", Remark: "", Level: 3, Pid: 115, Status: 1, Group: jp},
+
+			//---------------------------------------------------
+			//智干亚马逊德国数据
+			{Id: 119, Name: "de", Title: "基础数据", Remark: "", Level: 1, Pid: 0, Status: 1, Group: de},
+			{Id: 120, Name: "base/index", Title: "小类数据", Remark: "", Level: 2, Pid: 119, Status: 1, Group: de},
+			{Id: 121, Name: "Index", Title: "德国站小类数据列表", Remark: "", Level: 3, Pid: 120, Status: 1, Group: de},
+			{Id: 122, Name: "Query", Title: "德国站小类数据查询", Remark: "", Level: 3, Pid: 120, Status: 1, Group: de},
+			{Id: 123, Name: "Export", Title: "德国站小类数据导出", Remark: "", Level: 3, Pid: 120, Status: 1, Group: de},
+
+			{Id: 124, Name: "big/index", Title: "大类数据", Remark: "", Level: 2, Pid: 119, Status: 1, Group: de},
+			{Id: 125, Name: "Index", Title: "德国站大类数据列表", Remark: "", Level: 3, Pid: 124, Status: 1, Group: de},
+			{Id: 126, Name: "Query", Title: "德国站大类数据查询", Remark: "", Level: 3, Pid: 124, Status: 1, Group: de},
+			{Id: 127, Name: "Export", Title: "德国站大类数据导出", Remark: "", Level: 3, Pid: 124, Status: 1, Group: de},
+			{Id: 128, Name: "Asin", Title: "德国站Asin历史记录", Remark: "", Level: 3, Pid: 124, Status: 1, Group: de},
+
+			{Id: 129, Name: "asin/index", Title: "Asin数据", Remark: "", Level: 2, Pid: 119, Status: 1, Group: de},
+			{Id: 130, Name: "Index", Title: "德国站Asin数据列表", Remark: "", Level: 3, Pid: 129, Status: 1, Group: de},
+			{Id: 131, Name: "Query", Title: "德国站Asin数据查询", Remark: "", Level: 3, Pid: 129, Status: 1, Group: de},
+			{Id: 132, Name: "Export", Title: "德国站Asin数据导出", Remark: "", Level: 3, Pid: 129, Status: 1, Group: de},
+
+			{Id: 133, Name: "url/index", Title: "类目数据", Remark: "", Level: 2, Pid: 119, Status: 1, Group: de},
+			{Id: 1331, Name: "Index", Title: "德国站类目数据列表", Remark: "", Level: 3, Pid: 133, Status: 1, Group: de},
+			{Id: 1332, Name: "Query", Title: "德国站类目数据查询", Remark: "", Level: 3, Pid: 133, Status: 1, Group: de},
+			{Id: 134, Name: "Update", Title: "德国站类目数据更新", Remark: "", Level: 3, Pid: 133, Status: 1, Group: de},
+
+			//---------------------------------------------------
+			//智干亚马逊英国数据
+			{Id: 135, Name: "uk", Title: "基础数据", Remark: "", Level: 1, Pid: 0, Status: 1, Group: uk},
+			{Id: 136, Name: "base/index", Title: "小类数据", Remark: "", Level: 2, Pid: 135, Status: 1, Group: uk},
+			{Id: 137, Name: "Index", Title: "英国站小类数据列表", Remark: "", Level: 3, Pid: 136, Status: 1, Group: uk},
+			{Id: 138, Name: "Query", Title: "英国站小类数据查询", Remark: "", Level: 3, Pid: 136, Status: 1, Group: uk},
+			{Id: 139, Name: "Export", Title: "英国站小类数据导出", Remark: "", Level: 3, Pid: 136, Status: 1, Group: uk},
+
+			{Id: 140, Name: "big/index", Title: "大类数据", Remark: "", Level: 2, Pid: 135, Status: 1, Group: uk},
+			{Id: 141, Name: "Index", Title: "英国站大类数据列表", Remark: "", Level: 3, Pid: 140, Status: 1, Group: uk},
+			{Id: 142, Name: "Query", Title: "英国站大类数据查询", Remark: "", Level: 3, Pid: 140, Status: 1, Group: uk},
+			{Id: 143, Name: "Export", Title: "英国站大类数据导出", Remark: "", Level: 3, Pid: 140, Status: 1, Group: uk},
+			{Id: 144, Name: "Asin", Title: "英国站Asin历史记录", Remark: "", Level: 3, Pid: 140, Status: 1, Group: uk},
+
+			{Id: 145, Name: "asin/index", Title: "Asin数据", Remark: "", Level: 2, Pid: 135, Status: 1, Group: uk},
+			{Id: 146, Name: "Index", Title: "英国站Asin数据列表", Remark: "", Level: 3, Pid: 145, Status: 1, Group: uk},
+			{Id: 147, Name: "Query", Title: "英国站Asin数据查询", Remark: "", Level: 3, Pid: 145, Status: 1, Group: uk},
+			{Id: 148, Name: "Export", Title: "英国站Asin数据导出", Remark: "", Level: 3, Pid: 145, Status: 1, Group: uk},
+
+			{Id: 149, Name: "url/index", Title: "类目数据", Remark: "", Level: 2, Pid: 135, Status: 1, Group: uk},
+			{Id: 150, Name: "Index", Title: "英国站类目数据列表", Remark: "", Level: 3, Pid: 149, Status: 1, Group: uk},
+			{Id: 151, Name: "Query", Title: "英国站类目数据查询", Remark: "", Level: 3, Pid: 149, Status: 1, Group: uk},
+			{Id: 152, Name: "Update", Title: "英国站类目数据更新", Remark: "", Level: 3, Pid: 149, Status: 1, Group: uk},
+		}
+		nodes = append(nodes, amazonnode...)
 	}
 	for _, v := range nodes {
 		n := new(Node)
